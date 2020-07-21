095e939e60dc3504d68ec86cdd2f0b592d9360db
wanghui219 <274244485@qq.com>
2020-07-21 16:27:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/day11_ease_ui/RegistActivity.java
@@ -0,0 +1,13 @@
+package com.example.day11_ease_ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist);
+    }
+}
